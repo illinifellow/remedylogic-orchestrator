af832c4ef86c6009c7ce6d6919e05c98737e3634
andrey <truestep@gmail.com>
2020-08-27 17:42:12 +0300

    fixes

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ ENV APP_ENVIRONMENT=mvp1
 ENV AWS_ACCOUNT=dev
 
 #RUN apk update && apk add bash
+RUN apk update && apk add --no-cache openssh
 
 COPY . /app
 WORKDIR app
 RUN npm i
 
 EXPOSE 4000
-CMD npm run start
+CMD ./start.sh
---
+++ buildandrundocker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker container rm remedylogic-orchestrator
+docker image prune -f
+docker build --tag remedylogic-orchestrator .
+docker run --network="host" --publish 4000:4000 --detach --name remedylogic-orchestrator remedylogic-orchestrator
