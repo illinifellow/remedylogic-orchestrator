515ecdb1481cb4403d10d7decd94293f0c9d9b5e
Anton Dovbiy <45226934+aDovbiy@users.noreply.github.com>
2020-09-02 11:18:23 +0300

    fix to npm run start

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ else
 ssh -i "/root/keys/mongo_dev.pem" -L 0.0.0.0:37017:docdb-2020-07-09-06-28-43.cluster-cffqzqfck5m7.us-east-2.docdb.amazonaws.com:27017 ec2-user@ec2-18-223-210-51.us-east-2.compute.amazonaws.com -o "StrictHostKeyChecking no" -N -f;
 fi
 
-npm run start --port 80
+npm run start
