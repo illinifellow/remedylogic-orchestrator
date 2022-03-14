cd74dc25ff6d7dd9ba087f2e83ff23f994217f4c
andrey <truestep@gmail.com>
2022-03-14 14:26:00 +0200

    dotenv

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 const appVersion = 'v1'
 global.appVersion = appVersion
 global.appName = 'orchestrator'
+
+const result = dotenv.config()
+import dotenv from 'dotenv'
+if (result.error) {
+  throw result.error
+}
+console.log("dotenv:")
+console.log(JSON.stringify(result.parsed,0,1))
+
 import baseApp from '@remedy-logic/service-common'
 import getApiRoutes from './api/apiRoutes.js'
 import express from 'express'
